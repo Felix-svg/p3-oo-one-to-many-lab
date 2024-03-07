6344f7a1e3272535539e3f7c11f63bce82f2615c
Felix Omondi <136954351+Felix-svg@users.noreply.github.com>
2024-03-07 20:53:13 +0300

    Completed assignment

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,29 @@
 class Pet:
-    pass
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+
+    all = []
+    def __init__(self, name, pet_type, owner=None):
+        if pet_type in self.PET_TYPES:
+            self.name = name
+            self.pet_type = pet_type
+            self.owner = owner
+            Pet.all.append(self)
+        else:
+            raise Exception
 
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+        self.pets_list = []
+    def pets(self):
+        return Pet.all
+    
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise Exception
+        pet.owner = self
+        self.pets_list.append(pet)
+        
+    def get_sorted_pets(self):
+        return sorted(self.pets(), key=lambda pet: pet.name)
+
